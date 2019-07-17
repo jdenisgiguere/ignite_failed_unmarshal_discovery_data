77b7ef8a8d9ed17dbff46720fca24a2a90ae520c
Jean-Denis Giguere <jean-denis@anagraph.io>
2019-07-17 07:00:32 -0400

    docker-compose to reproduce with ignite 2.7.5
    Signed-off-by: Jean-Denis Giguere <jean-denis@anagraph.io>

--- src/main/scala/Cacher.scala
+++ src/main/scala/Cacher.scala
@@ -1,12 +1,28 @@
-import org.apache.ignite.configuration.CacheConfiguration
+import java.util
+
+import org.apache.ignite.configuration.{CacheConfiguration, IgniteConfiguration}
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder
 import org.apache.ignite.{Ignite, IgniteCache, Ignition}
+
 
 object Cacher {
   case class MyString(text: String)
   def main(args: Array[String]): Unit = {
     println("Starting...")
 
-    val ignite: Ignite = Ignition.start("ignite-spring-config.xml")
+    val igniteCfg: IgniteConfiguration = new IgniteConfiguration()
+    igniteCfg.setClientMode(true)
+
+    val discoverySpi: TcpDiscoverySpi = new TcpDiscoverySpi()
+    discoverySpi.setJoinTimeout(10000)
+    val ipFinder: TcpDiscoveryVmIpFinder = new TcpDiscoveryVmIpFinder()
+    val ips: util.List[String] = util.Arrays.asList("ignite")
+    ipFinder.setAddresses(ips)
+    discoverySpi.setIpFinder(ipFinder)
+    igniteCfg.setDiscoverySpi(discoverySpi)
+
+    val ignite: Ignite = Ignition.start(igniteCfg)
     val cacheCfg = new CacheConfiguration[String, MyString]()
     cacheCfg.setName("mystringcache")
     val cache: IgniteCache[String, MyString] = ignite.getOrCreateCache(cacheCfg)
